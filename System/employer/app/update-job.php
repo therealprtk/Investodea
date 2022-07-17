dbcd66d97a01b58129a9de62ed1a71863364ddc6
xiaoganyu23 <107012640+xiaoganyu23@users.noreply.github.com>
2022-07-17 14:28:54 +0530

    b

--- System/employer/app/update-job.php
+++ System/employer/app/update-job.php
@@ -4,14 +4,10 @@ require '../constants/check-login.php';
 
 $job_id = $_POST['jobid'];
 $title  = ucwords($_POST['title']);
-$city  = ucwords($_POST['city']);
 $state = $_POST['state'];
 $category = $_POST['category'];
 $type = $_POST['jobtype'];
-$exp = $_POST['experience'];
 $desc = ucfirst($_POST['description']);
-$rec = ucfirst($_POST['requirements']);
-$res = ucfirst($_POST['responsiblities']);
 $deadline = $_POST['deadline'];
 
 try {
@@ -19,16 +15,12 @@ $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	
-$stmt = $conn->prepare("UPDATE ideas SET title = :title, city = :city, state = :state, category = :category, type = :type, experience = :experience, description = :description, responsibility = :responsibility, requirements = :requirements WHERE job_id = :jobid AND company = '$myid'");
+$stmt = $conn->prepare("UPDATE ideas SET title = :title, state = :state, category = :category, type = :type,  description = :description, WHERE job_id = :jobid AND company = '$myid'");
 $stmt->bindParam(':title', $title);
-$stmt->bindParam(':city', $city);
 $stmt->bindParam(':state', $state);
 $stmt->bindParam(':category', $category);
 $stmt->bindParam(':type', $type);
-$stmt->bindParam(':experience', $exp);
 $stmt->bindParam(':description', $desc);
-$stmt->bindParam(':responsibility', $res);
-$stmt->bindParam(':requirements', $rec);
 $stmt->bindParam(':jobid', $job_id);
 $stmt->execute();
 
