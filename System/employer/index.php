d7893669470497fdf688397d6bdbdf6429e06a0b
Pratik Singh Nag <therealprtk@gmail.com>
2022-07-17 12:00:39 +0530

    h

--- System/employer/index.php
+++ System/employer/index.php
@@ -32,7 +32,7 @@ header("location:../");
     <meta property="og:image:alt" content="Bwire Jobs" />
     <meta property="og:description" content="Online Job Management / Job Portal" />
 
-	<link rel="shortcut icon" href="../images/ico/favicon.png">
+	<link rel="shortcut icon" href="../images/ico/logoinv.png">
 
 	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css" media="screen">	
 	<link href="../css/animate.css" rel="stylesheet">
