cee3e4745c23a017d9ec416e43268e5ca4081fad
Pratik Singh Nag <therealprtk@gmail.com>
2022-07-17 14:32:22 +0530

    as

--- System/employer/change-password.php
+++ System/employer/change-password.php
@@ -67,7 +67,7 @@ header("location:../");
 					
 					<div class="logo-wrapper">
 						<div class="logo">
-							<a href="../"><img src="../images/logo.png" alt="Logo" /></a>
+							<a href="../"><img src="../images/inv.png" alt="Logo" /></a>
 						</div>
 					</div>
 					
