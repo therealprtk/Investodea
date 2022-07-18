21241fb8bab35592167b2a37291725d920a59f4e
xiaoganyu23 <107012640+xiaoganyu23@users.noreply.github.com>
2022-07-18 05:43:09 +0530

    .

--- System/constants/settings.php
+++ System/constants/settings.php
@@ -2,9 +2,9 @@
 $actual_link = $_SERVER['HTTP_HOST'];
 
 $default_timezone = '';
-$smtp_host = '';
-$smtp_user = '';
-$smtp_pass = '';
+$smtp_host = 'smtp.gmail.com';
+$smtp_user = 'investodea@gmail.com';
+$smtp_pass = 'investorideator';
 
 $contact_mail = '';
 
